e6d5d0817eff69ec32d15a06eb304eccc22eef67
Diego Jasso <diego.jasso@auryc.com>
2020-02-07 14:21:23 -0800

    installed GTM gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,27 @@ module.exports = {
         icon: `src/images/oscar-icon.png`, // This path is relative to the root of the site.
       },
     },
+    // https://www.gatsbyjs.org/packages/gatsby-plugin-google-tagmanager/
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-KB4485V",
+  
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+  
+        // Specify optional GTM environment details.
+        gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        dataLayerName: "YOUR_DATA_LAYER_NAME",
+      },
+    },
     // https://www.gatsbyjs.org/packages/gatsby-plugin-google-analytics/
     {
         resolve: `gatsby-plugin-google-analytics`,
